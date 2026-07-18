735c36564e5b932e634ed65f649a5adb0d37437e
hannayou1 <pooh9868@naver.com>
2026-07-18 14:32:13 +0900

    18강 연락처 완료

--- src/components/Contact02.jsx
+++ src/components/Contact02.jsx
@@ -1,7 +1,59 @@
 import React from "react";
 
+const contactText = [
+    {
+        link: "https://open.kakao.com/o/gM7YLzwf",
+        title: "KAKAO : webstupids",
+    },
+    {
+        link: "mailto:webstoryboy@naver.com",
+        title: "mail : webstoryboy@naver.com",
+    },
+];
+
 const Contact02 = () => {
-  return <div>Contact02</div>;
+  return (
+    <section id="contact">
+            <div className="contact__inner">
+                <h2 className="contact__title">Contact</h2>
+                <div className="contact__lines top" aria-hidden="true">
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                </div>
+                <div className="contact__text">
+                    <div className="text">
+						{contactText.map((contact, key) => (
+							<div key={key}>
+								<a 
+									href={contact.link} 
+									target="_blank" 
+									rel="noopnener noreferrer"
+									>
+										{contact.title}
+								</a>
+							</div>
+						))}
+
+                        
+                    </div>
+                </div>
+                <div className="contact__lines" aria-hidden="true">
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                </div>
+            </div>
+    </section>
+  );
 };
 
 export default Contact02;
